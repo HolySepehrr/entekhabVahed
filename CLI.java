d45d950f8d393d839e84f4c662daa0ec6d0e566c
sepehr <sepehr.fallah@gmail.com>
2024-03-05 01:18:12 +0330

    sakht avalin daaneshkade va dibag

--- CLI.java
+++ CLI.java
@@ -1,2 +1,23 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CLI {
+
+    public CLI() {
+        HashMap<String, LocalTime[]> Set = new HashMap<>();
+
+        General riazi1 = new General("moghadasi", "riazi1", "1403/04/03", "08:00", "11:00", 3069, 100, 4);
+        riazi1.tabdil("shanbe", "08:00", "10:00");
+        riazi1.tabdil("doshanbe", "08:00", "10:00");
+
+
+        ArrayList<General> MathGenerals = new ArrayList<>();
+        ArrayList<Professional> MathProfessionals = new ArrayList<>();
+
+        MathGenerals.add(riazi1);
+        College Math = new College(MathGenerals, MathProfessionals);
+    }
+
+
 }
