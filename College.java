bd4fd9c33394d9ef11cfe81e5880eea8eed25bc4
sepehr <sepehr.fallah@gmail.com>
2024-03-04 22:07:33 +0330

    Constructor added to College

--- College.java
+++ College.java
@@ -1,5 +1,10 @@
 public class College {
 
+    public College(General[] general, Professional[] professionals) {
+        this.general = general;
+        this.professionals = professionals;
+    }
+
     General[] general;
     Professional[] professionals;
 
