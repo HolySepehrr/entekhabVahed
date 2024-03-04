c5a77f49aaf1b64f77baa244c67c791f786fd3c5
sepehr <sepehr.fallah@gmail.com>
2024-03-04 21:38:49 +0330

    Course por shod

--- Course.java
+++ Course.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+
 public class Course {
+    String ostad;
+    String dars;
+    int [] emtahan = new int[7];
+    int code;
+    int zarfiat;
+    int vahed;
+    HashMap <String , Integer[]> time;
+
 }
